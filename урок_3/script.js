47333af7925c4fb3d3746705ce9fdcfe9c381064
Сулейман <59031620+Suleim06@users.noreply.github.com>
2022-12-30 13:17:48 +0300

    Add files via upload

---
+++ урок_3/script.js
@@ -0,0 +1,13 @@
+let name = prompt("Как вас зовут?");
+alert("Вас зовут " + name);
+
+let number = prompt("Любое число");
+if (number > 0){
+    alert(1);
+}
+else if(number < 0){
+    alert(-1);
+}
+else{
+    alert(0);
+}
