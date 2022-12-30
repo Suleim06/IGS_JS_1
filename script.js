5881b13ec26da6f91d00959e9ad67adb483b67cc
Сулейман <59031620+Suleim06@users.noreply.github.com>
2022-12-30 12:57:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let name = "Hemfix";
+let number = 14;
+let age = 19;
+let someAnimals = "Кот, Конь, Волк";
+let boolean = true;
+console.log(typeof name);
+console.log(typeof number);
+console.log(typeof someAnimals);
+console.log(name.length);
+console.log("Меня зовут " + name);
+console.log("Мне " + age + " лет");
